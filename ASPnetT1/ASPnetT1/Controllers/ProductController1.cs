d7066f0252391a2988c08416668c06379cf590f6
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-05-24 01:23:06 +0400

    crated file

---
+++ ASPnetT1/ASPnetT1/Controllers/ProductController1.cs
@@ -0,0 +1,20 @@
+﻿using ASPnetT1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASPnetT1.Controllers
+{
+    public class ProductController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product { Id = 1, Name = "BMW", Description="d1",Image="p1.jpg",Price=50},
+                new Product { Id = 2, Name = "Audi", Description="d1",Image="p2.jpg",Price=100},
+                new Product { Id = 3, Name = "Kia", Description="d1",Image="p3.jpg",Price=150}
+            };
+            return View(products);
+        }
+    }
+}
