694a7034925c880c7f3e4149865d4bd366dd5bb5
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-06-07 02:21:56 +0400

    created task

---
+++ Alfa1/Create task/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Create_task.Controllers
+{
+    public class AccountController:Controller
+    {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public AccountController(RoleManager<IdentityRole>
+            roleManager,UserManager<AppUser>userManager,SignInManager<AppUser>
+            signInManager)
+        {
+            _userManager = userManager;
+            _signManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async void CreateRole()
+        {
+            foreach (var item in Enum.GetValues(typeof(Roless)))
+            {
+                if (!await _roleManager.RoleExistsAsync(item.ToString()))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole { Name = item.ToString() });
+                }
+            }
+        }
+    }
+}
---
+++ Alfa1/Create task/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Create_task.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Create_task.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
